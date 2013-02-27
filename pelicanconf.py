620e1f293cd097ef0ebd457fd4c0d127a98c54d4
Julozi <julien.seiler@gmail.com>
2013-02-27 10:03:09 +0100

    initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Julien Seiler'
+SITENAME = u"coalkids' blog"
+SITEURL = 'http://coalkids.github.com'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS =  (('Bulleval', 'http://www.bulleval.fr'),)
+
+# Social widget
+SOCIAL = (('github', 'http://github.com/coalkids'),)
+
+DEFAULT_PAGINATION = 10
